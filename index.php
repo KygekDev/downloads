e42cbe06dadcb3cda2e88f22678d5d6420dd3bca
Kygekraqmak <techfixindonesia@gmail.com>
2020-08-01 20:43:07 +0800

    Added important files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Downloads - Kygekraqmak</title>
+  </head>
+  <body>
+    <h1>Kygekraqmak's Projects Download Center</h1>
+    <hr />
+    <h4>KygekRulesUI (A RulesUI plugin by Kygekraqmak)</h4>
+    <h4><a href="https://kygekraqmak.github.io/downloads/kygekrulesui">Downloads</a></h4>
+  </body>
+</html>
---
+++ kygekrulesui.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>KygekRulesUI - Downloads</title>
+  </head>
+  <body>
+    <h1>Kygekraqmak's Projects Download Center</h1>
+    <hr />
+    <h4>Version 1.0.1 (Released 2 August 2020)</h4>
+    <h4><a href="https://kygekraqmak.github.io/downloads/kygekrulesui/KygekRulesUI_v1.0.1.phar">Download</a></h4>
+    <h4>Changes:</h4>
+    <h4>- Config automatically gets reloaded when /rules is executed.</h4>
+    <h4>- Fixed no permission on non-op players</h4>
+    <hr />
+    <h4>Version 1.0.0 (Released 1 August 2020)</h4>
+    <h4><a href="https://kygekraqmak.github.io/downloads/kygekrulesui/KygekRulesUI_v1.0.0.phar">Download</a></h4>
+    <h4>Changes:</h4>
+    <h4>- Initial release</h4>
+  </body>
+</html>
